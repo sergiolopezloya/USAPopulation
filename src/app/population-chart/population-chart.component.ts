28b585de98ce34bdb8847a63340ced45873291e7
Sergio Lopez <sergio.lopez@divelement.io>
2023-09-07 20:35:08 -0600

    Add population data service

--- src/app/population-chart/population-chart.component.ts
+++ src/app/population-chart/population-chart.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PopulationDataService } from '../population-data.service';
 
 @Component({
   selector: 'app-population-chart',
   templateUrl: './population-chart.component.html',
   styleUrls: ['./population-chart.component.scss']
 })
-export class PopulationChartComponent {
+export class PopulationChartComponent implements OnInit {
+  constructor(private populationDataService: PopulationDataService) {}
 
+  ngOnInit(): void {
+    this.populationDataService.getPopulationData('Nation').subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
---
+++ src/app/population-data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PopulationDataService {
+  private apiUrl = 'https://datausa.io/api/data';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getPopulationData(drilldown: 'Nation' | 'State'): Observable<any> {
+    const params = new HttpParams().set('drilldowns', drilldown).set('measures', 'Population');
+
+    return this.httpClient.get(this.apiUrl, { params });
+  }
+}
