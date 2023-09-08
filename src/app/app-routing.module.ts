2a7e82f008996aa31a53850f0b3e83658c960fe8
Sergio Lopez <sergio.lopez@divelement.io>
2023-09-07 21:12:09 -0600

    Personalize home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { HomeComponent } from './home/home.component';
 import { PopulationChartComponent } from './population-chart/population-chart.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'population', component: PopulationChartComponent }
+  { path: '', component: HomeComponent, title: 'Home' },
+  { path: 'population', component: PopulationChartComponent, title: 'Population' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  static routes: Routes[] = [];
+  constructor() {
+    AppRoutingModule.routes.push(routes);
+  }
+}
